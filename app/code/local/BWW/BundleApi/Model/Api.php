c0902b946dc2c273cfd0b9b7d1c3761e75254769
Retchel Tapayan <rtapayan@global-fusion.net>
2014-10-07 17:39:22 +0800

    Bundle IDs

--- app/code/local/BWW/BundleApi/Model/Api.php
+++ app/code/local/BWW/BundleApi/Model/Api.php
@@ -1,42 +1,42 @@
 <?php
-class BWW_BundleApi_Model_Api extends Mage_Api_Model_Resource_Abstract
+class BWW_BundleApi_Model_Api 
 {
 
-    public function createSelectionLink($items, $selectionRawData, $productId, $storeid)
-    {
+    // public function createSelectionLink($items, $selectionRawData, $productId, $storeid)
+    // {
 
-        $selections = array();
+    //     $selections = array();
 
-        //check if product id in selection data is valid
-        $optionProduct = Mage::getModel('catalog/product')->load($selectionRawData['product_id']);
+    //     //check if product id in selection data is valid
+    //     $optionProduct = Mage::getModel('catalog/product')->load($selectionRawData['product_id']);
 
-        if(!$optionProduct->getId()){
-            throw new Exception('Selection product provided does not reference a valid product');
-        }
+    //     if(!$optionProduct->getId()){
+    //         throw new Exception('Selection product provided does not reference a valid product');
+    //     }
 
-        $selections[] = $selectionRawData;
+    //     $selections[] = $selectionRawData;
 
-        $product   = Mage::getModel('catalog/product')->setStoreId($storeid);
-        $product->load($productId);
+    //     $product   = Mage::getModel('catalog/product')->setStoreId($storeid);
+    //     $product->load($productId);
 
-        if (!$product->getId()) {
-            //bail
-            throw new Exception('Product loaded does not exist');
-        }
+    //     if (!$product->getId()) {
+    //         //bail
+    //         throw new Exception('Product loaded does not exist');
+    //     }
 
-        Mage::register('product', $product);
-        Mage::register('current_product', $product);
+    //     Mage::register('product', $product);
+    //     Mage::register('current_product', $product);
 
-        $product->setCanSaveConfigurableAttributes(false);
-        $product->setCanSaveCustomOptions(true);
+    //     $product->setCanSaveConfigurableAttributes(false);
+    //     $product->setCanSaveCustomOptions(true);
 
-        $product->setBundleOptionsData($items);
-        $product->setBundleSelectionsData($selections);
-        $product->setCanSaveCustomOptions(true);
-        $product->setCanSaveBundleSelections(true);
+    //     $product->setBundleOptionsData($items);
+    //     $product->setBundleSelectionsData($selections);
+    //     $product->setCanSaveCustomOptions(true);
+    //     $product->setCanSaveBundleSelections(true);
 
-        $product->save();
-    }
+    //     $product->save();
+    // }
 
 
 
